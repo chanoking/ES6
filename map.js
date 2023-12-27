35c6cfe7339f7b930e3ff632a94224258509817b
진찬호 <chanhojin94@kakao.com>
2023-12-27 21:24:13 +0900

    map

---
+++ map.js
@@ -0,0 +1,22 @@
+const arr = [1, 2, 3, 4];
+const newArr = [];
+// const newNewArr = arr.forEach((e) => newArr.push(e * 2));
+// console.log(newArr);
+// console.log(arr);
+
+// const newArrr = arr.map((e) => e * 2);
+// console.log(newArrr);
+
+const player = [
+  {
+    name: "son",
+    age: 29,
+  },
+  {
+    name: "Ronaldo",
+    age: 35,
+  },
+];
+
+const hi = player.map((p) => p.name);
+console.log(hi);
